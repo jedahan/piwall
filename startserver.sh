a436577f8fffbc2b1ace82a736bc6eed5429e4fb
Jonathan Dahan <jonathan@jedahan.com>
2014-07-07 11:18:42 -0400

    infinite loop the video

--- startserver.sh
+++ startserver.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-sleep 15
-avconv -re -i /shared/test.avi -vcodec copy -an -f avi udp://239.0.1.23:1234
+sleep 7
+while :
+do
+    avconv -re -i /shared/test.avi -vcodec copy -an -f avi udp://239.0.1.23:1234
+done
