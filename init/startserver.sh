a095f4c0f53f77d283130f33512fccb1704ee23a
Jonathan Dahan <jonathan@jedahan.com>
2014-07-16 15:27:23 -0400

    Make avconv start quicker
    -analyzeduration 0 and -fpsprobe 0 help with the restart

--- init/startserver.sh
+++ init/startserver.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-while :
-do
-    avconv -re -i /shared/test.avi -vcodec copy -an -f avi udp://239.0.1.23:1234
+while true; do
+    avconv -fpsprobesize 0 -analyzeduration 0 -re -i /shared/test.h264 -c copy -f h264 udp://239.0.1.23:1234
 done
